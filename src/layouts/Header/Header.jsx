cd82f799b779092ca6670e36becaba13d90508dc
atrocityz <disbalance17@Mail.ru>
2025-03-03 22:15:24 +0700

    initial commit + header implementation without mobile adaptation

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,61 @@
+import './Header.scss'
+import Logo from '@/components/Logo'
+import classNames from 'classnames'
+import Button from '@/components/Button'
+
+const Header = (props) => {
+  const { url } = props
+
+  const menuItems = [
+    {
+      label: 'Home',
+      href: '/'
+    },
+    {
+      label: 'Movies & Shows',
+      href: '/movies'
+    },
+    {
+      label: 'Support',
+      href: '/support'
+    },
+    {
+      label: 'Subscriptions',
+      href: '/subscriptions'
+    }
+  ]
+
+  return <header className="header">
+    <div className="header__inner container">
+      <Logo loading="eager" />
+      <nav className="header__menu">
+        <ul className="header__menu-list">
+          {menuItems.map(({ label, href }, index) => (
+            <li className="header__menu-item" key={index}>
+              <a className={classNames('header__menu-link', {
+                'is-active': href === url
+              })} href={href}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="header__actions">
+        <Button
+          className="header__button"
+          label="Search"
+          iconName="search"
+          mode="transparent"
+        />
+        <Button
+          className="header__button"
+          label="Notifications"
+          iconName="notification"
+          mode="transparent"
+        />
+      </div>
+
+    </div>
+  </header>
+}
+
+export default Header
