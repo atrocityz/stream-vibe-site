ef6c1d7d9773ffe57e5c504a51be23d775cc2473
atrocityz <disbalance17@Mail.ru>
2025-03-04 09:20:16 +0700

    added header mobile adaptation + burger menu

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -2,6 +2,7 @@ import './Header.scss'
 import Logo from '@/components/Logo'
 import classNames from 'classnames'
 import Button from '@/components/Button'
+import BurgerButton from '@/components/BurgerButton'
 
 const Header = (props) => {
   const { url } = props
@@ -25,37 +26,49 @@ const Header = (props) => {
     }
   ]
 
-  return <header className="header">
-    <div className="header__inner container">
-      <Logo loading="eager" />
-      <nav className="header__menu">
-        <ul className="header__menu-list">
-          {menuItems.map(({ label, href }, index) => (
-            <li className="header__menu-item" key={index}>
-              <a className={classNames('header__menu-link', {
-                'is-active': href === url
-              })} href={href}>{label}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="header__actions">
-        <Button
-          className="header__button"
-          label="Search"
-          iconName="search"
-          mode="transparent"
-        />
-        <Button
-          className="header__button"
-          label="Notifications"
-          iconName="notification"
-          mode="transparent"
+  return (
+    <header className="header" data-js-overlay-menu="">
+      <div className="header__inner container">
+        <Logo loading="eager" />
+        <dialog
+          className="header__overlay-menu-dialog"
+          data-js-overlay-menu-dialog=""
+        >
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({ label, href }, index) => (
+                <li className="header__menu-item" key={index}>
+                  <a className={classNames('header__menu-link', {
+                    'is-active': href === url
+                  })} href={href}>{label}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button
+              className="header__button"
+              label="Search"
+              iconName="search"
+              mode="transparent"
+            />
+            <Button
+              className="header__button"
+              label="Notifications"
+              iconName="notification"
+              mode="transparent"
+            />
+          </div>
+        </dialog>
+        <BurgerButton
+          className="header__burger-button visible-tablet"
+          extraAttrs={{
+            'data-js-overlay-menu-burger-button': ''
+          }}
         />
       </div>
-
-    </div>
-  </header>
+    </header>
+  )
 }
 
 export default Header
